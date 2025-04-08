ede9725babbbff746e4a7e5e3cc19e0054e27bf0
bt00000 <bobthecat1020@gmail.com>
2025-04-08 12:30:58 -0700

    Calculate Avg CPU and Underutlized condition

--- lambda/main.py
+++ lambda/main.py
@@ -11,7 +11,7 @@ def lambda_handler(event, context):
     response = ec2.describe_instances(Filters=[
         {"Name": "instance-state-name", "Values": ["running"]}
     ])
-    underutilized = []
+    underutilized_instances = []
 
     for reservation in response["Reservations"]:
         for instance in reservation["Instances"]:
@@ -19,10 +19,16 @@ def lambda_handler(event, context):
             
             cpu = cloudwatch.get_metric_statistics(
                 Namespace="AWS/EC2",
-                MetricName="CPUUtilization"
+                MetricName="CPUUtilization",
                 Dimensions=[{"Name": "InstanceId", "Value": instance_id}], # Give stats for this specific instance ID
                 StartTime=datetime.utcnow() - timedelta(hours=24), # StartTime/EndTime checks last 24 hours
                 EndTime=datetime.utcnow(),
                 Period = 3600, # Every 1 hour
                 Statistics=["Average"] # Get average CPU % in each period
             )
+
+            datapoints = cpu["Datapoints"]
+            if datapoints:
+                avg_cpu = sum(dp["Average"] for dp in datapoints / len(datapoints))
+                if avg_cpu < 10: # If average CPU < 10%
+                    underutilized_instances.append((instance_id, round(avg_cpu, 2))) # Mark it as underutilized and add to list

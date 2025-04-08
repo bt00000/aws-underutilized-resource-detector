d8ade316a10e991e676bcf187b806b7bf9f3af00
bt00000 <bobthecat1020@gmail.com>
2025-04-07 17:40:31 -0700

    Adding sns publish policy permission

--- main.tf
+++ main.tf
@@ -17,6 +17,11 @@ resource "aws_iam_role" "lambda_exec_role" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "attach_sns_publish_policy" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.sns_publish_policy.arn
+}
+
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"

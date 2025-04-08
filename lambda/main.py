ed570a998086dcf48b2ba1f5c06abdf2a98ffdaa
bt00000 <bobthecat1020@gmail.com>
2025-04-08 12:23:42 -0700

    Check Instances CPU usage for last 24 hours

--- lambda/main.py
+++ lambda/main.py
@@ -1,19 +1,28 @@
 import os
 import boto3
+from datetime import datetime, timedelta
 
 def lambda_handler(event, context):
     sns_arn = os.environ.get("SNS_TOPIC_ARN")
-    
-    message = "TESTING: Lambda ran successfully!"
-    
+    cloudwatch = boto3.client("cloudwatch")
+    ec2 = boto3.client("ec2")
     sns = boto3.client("sns")
-    sns.publish(
-        TopicArn=sns_arn,
-        Subject="Test: Lambda Alert",
-        Message=message
-    )
-    
-    return {
-        "statusCode": 200,
-        "body": "Notification sent."
-    }
+
+    response = ec2.describe_instances(Filters=[
+        {"Name": "instance-state-name", "Values": ["running"]}
+    ])
+    underutilized = []
+
+    for reservation in response["Reservations"]:
+        for instance in reservation["Instances"]:
+            instance_id = instance["InstanceId"]
+            
+            cpu = cloudwatch.get_metric_statistics(
+                Namespace="AWS/EC2",
+                MetricName="CPUUtilization"
+                Dimensions=[{"Name": "InstanceId", "Value": instance_id}], # Give stats for this specific instance ID
+                StartTime=datetime.utcnow() - timedelta(hours=24), # StartTime/EndTime checks last 24 hours
+                EndTime=datetime.utcnow(),
+                Period = 3600, # Every 1 hour
+                Statistics=["Average"] # Get average CPU % in each period
+            )
